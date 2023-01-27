56dcbd54ee46df137825a98adf12f7615339f85f
Kassandra Johnson <kassandra.m.johnson@gmail.com>
2023-01-27 11:03:19 -0500

    reuploading - failed to include actual work last push

---
+++ Testing/Models/ProductRepo.cs
@@ -0,0 +1,55 @@
+﻿using Dapper;
+using System.Collections.Generic;
+using System.Data;
+
+namespace Testing.Models
+{
+    public class ProductRepo : IProductRepo
+    {
+        private readonly IDbConnection _conn;
+        public ProductRepo(IDbConnection conn)
+        {
+            _conn = conn;
+        }
+
+        public IEnumerable<Product> GetAllProducts()
+        {
+            return _conn.Query<Product>("SELECT * FROM PRODUCTS;");
+        }
+
+        public Product GetProduct(int id)
+        {   
+            return _conn.QuerySingle<Product>("SELECT * FROM PRODUCTS WHERE PRODUCTID = @id", new { id = id });
+        }
+
+        public void UpdateProduct(Product product)
+        {
+            _conn.Execute("UPDATE products SET Name = @name, Price = @price, OnSale = @onsale, StockLevel = @stocklevel WHERE ProductID = @id",
+                new { name = product.Name, price = product.Price, onsale = product.OnSale, stocklevel = product.StockLevel, id = product.ProductID });
+        }
+
+        public Product AssignCategory()
+        {
+            var categoryList = GetCategories();
+            var product = new Product();
+            product.Categories = categoryList;
+            return product;
+        }
+
+        public IEnumerable<Category> GetCategories()
+        {
+            return _conn.Query<Category>("SELECT * FROM categories;");
+        }
+        public void InsertProduct(Product productToInsert)
+        {
+            _conn.Execute("INSERT INTO products (NAME, PRICE, CATEGORYID) VALUES (@name, @price, @categoryID);",
+                new { name = productToInsert.Name, price = productToInsert.Price, categoryID = productToInsert.CategoryID });
+        }
+        public void DeleteProduct(Product product)
+        {
+            _conn.Execute("DELETE FROM REVIEWS WHERE ProductID = @id;", new { id = product.ProductID });
+            _conn.Execute("DELETE FROM Sales WHERE ProductID = @id;", new { id = product.ProductID });
+            _conn.Execute("DELETE FROM Products WHERE ProductID = @id;", new { id = product.ProductID });
+        }
+    }
+}
